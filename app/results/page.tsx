d7f2a22fc66f05dbf7d463776ff3a22075878471
akileshbha <akileshbha@gmail.com>
2025-11-17 18:35:49 +0000

    survey project init commit with next.js, mysql, drizzl e orm

---
+++ app/results/page.tsx
@@ -0,0 +1,14 @@
+import Link from 'next/link';
+
+export const metadata = {
+    title: 'Results',
+    description: 'Survey results page',
+};
+
+export default function Results() {
+    return (
+        <>
+            <h1>hi this is results</h1>
+        </>
+    );
+}
---
+++ lib/db.ts
@@ -0,0 +1,5 @@
+// import 'dotenv/config';
+import { drizzle } from "drizzle-orm/mysql2";
+import * as schema from "../db/schema";
+
+const db = drizzle(process.env.DATABASE_URL);
