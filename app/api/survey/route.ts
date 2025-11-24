04072c220273b22c5e7c94a6a2c55dd65274519d
akileshbha <akileshbha@gmail.com>
2025-11-24 18:48:54 +0000

    removed stack from looging into erroe log

--- app/api/survey/route.ts
+++ app/api/survey/route.ts
@@ -29,7 +29,7 @@ export async function POST(req: Request) {
         // Always return JSON, never plain text
         await logError({
             message: err.message,
-            stack: err.stack,
+            // stack: err.stack,
             service: "server",
             level: "error",
             path: req.url,
